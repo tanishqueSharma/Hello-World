1d2e5191201653ee79dc5358865098afbd13a5e7
Tanishque <tanishque.sharma@gmail.com>
2023-09-01 17:35:04 +0530

    first commit

---
+++ src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <h1>{{msg}}</h1>
+</template>
+
+<script>
+export default {
+  name: 'App',
+  
+  data(){
+    return{
+      msg:'Hello World!'
+    }
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
