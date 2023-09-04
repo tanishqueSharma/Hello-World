673227747914a0b485fa55b13a02ce26f5415163
tanishqueSharma <142586530+tanishqueSharma@users.noreply.github.com>
2023-09-04 10:07:51 +0530

    Update LoginPage.vue

--- src/components/LoginPage.vue
+++ src/components/LoginPage.vue
@@ -1,6 +1,9 @@
 <template>
     <div>
         <h1>Login Page</h1>
+        <input type="text" placeholder="Enter name"/>
+    <input type="password" placeholder="Enter password"/>
+    <button>Login</button>
     </div>
 </template>
 
